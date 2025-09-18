bc08fa8b14f1b46a5a8ca9bb7429d3a794d495aa
pdheeraj99 <pilladheeraj@gmail.com>
2025-09-18 11:43:57 +0530

    Removed AOP complete

--- Spring-Project/src/main/java/io/mawa/spring/core/aop/proxies/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package io.mawa.spring.core.aop.proxies;
-
-/**
- * Mawa, this is the interface (the "rulebook").
- * Beans that implement this will be proxied by JDK Dynamic Proxy.
- */
-public interface PaymentService {
-    void makePayment(double amount);
-}
